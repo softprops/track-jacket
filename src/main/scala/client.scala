42be7e7c48dbf0b22684955ba305f3f66e5c74a9
softprops <d.tangren@gmail.com>
2014-07-08 01:13:47 -0400

    small sync

--- src/main/scala/client.scala
+++ src/main/scala/client.scala
@@ -1,15 +1,14 @@
 package trackjacket
 
-import com.ning.http.client.Response
-import dispatch._, dispatch.Defaults._
+import com.ning.http.client.{ AsyncHandler, Response }
+import dispatch.{ :/, Http, Req }
 import org.json4s.JsonDSL._
 import org.json4s.native.JsonMethods.render
 import org.json4s.native.Printer.compact
-import scala.concurrent.Future
+import scala.concurrent.{ ExecutionContext, Future }
 
 object Client {
-
-  type Handler[T] = Response => T
+  type Handler[T] = AsyncHandler[T]
 
   val Agent = s"Track-jacket/${BuildInfo.version}"
 
@@ -26,7 +25,6 @@ object Client {
   object Default {
     def port = 8080
     val host = "localhost"
-    val credentials: Option[(String, String)] = None
     val mem = 128.0
     val cpus = 1.0
     val instances = 1
@@ -35,18 +33,23 @@ object Client {
   }
 }
 
-// v2 client
 case class Client(
   host: String = Client.Default.host,
   port: Int = Client.Default.port,
-  credentials: Option[(String, String)] = Client.Default.credentials,
-  http: Http = new Http) {
+  credentials: Option[(String, String)] = None,
+  http: Http = new Http)
+  (implicit ec: ExecutionContext) {
+
+  def close() = http.shutdown()
+
 
   private def base = :/(host, port) / "v2" <:< Client.Headers
 
-  private def request[T](req: Req)(handler: Client.Handler[T] = as.Unit): Future[T] =
-    http(credentials.map { case (user, pass) => req.as_!(user, pass) }
-                    .getOrElse(req) OK handler)
+  private def request[T]
+    (req: Req)
+    (handler: Client.Handler[T] = as.Unit): Future[T] =
+      http(credentials.map { case (user, pass) => req.as_!(user, pass) }
+           .getOrElse(req) OK handler)
 
   private def complete(req: Req): Client.Completion =
     new Client.Completion {
@@ -56,7 +59,7 @@ case class Client(
 
   case class Apps(_cmd: Option[String] = None) extends Client.Completion {
     def cmd(str: String) = copy(_cmd = Some(str))
-    override def apply[T](handler: Client.Handler[T] = as.Unit) =
+    override def apply[T](handler: Client.Handler[T] = as.Apps) =
       request(base / "apps" <<? Map.empty[String, String] ++ _cmd.map("command" -> _))(handler)
   }
 
@@ -65,6 +68,7 @@ case class Client(
     taskId: String,
     _scale: Boolean = false)
     extends Client.Completion {
+    def scalaDown = copy(_scale = true)
     override def apply[T](handler: Client.Handler[T] = as.Unit) =
       request(base.DELETE / "apps" / appId / "tasks" / taskId
               <<? Map("scale" -> _scale.toString))(handler)
@@ -84,21 +88,48 @@ case class Client(
   }
 
   case class Container(image: String, options: List[String])
-  case class Constraint(attr: String, operator: String, value: Option[String] = None)
+
+  object Constraints {
+    sealed trait Constraint {
+      def op: String
+      def field: String
+      def value: Option[String] = None
+    }
+
+    /**  to enforce uniqueness of the attribute across all of an app’s tasks
+     *   Unique(hostname) */
+    case class Unique(field: String) extends Constraint {
+      val op = "UNIQUE"
+    }
+
+    /** run all of your app’s tasks on slaves that share a certain attribute
+     *  Cluster("rack_id", "rack-1") */
+    case class Cluster(field: String, _value: String) extends Constraint {
+      val op = "CLUSTER"
+      override val value = Some(_value)
+    }
+
+    /** distribute your tasks evenly across racks or datacenters for high availability
+     *  GroupBy("rack_id") */
+    case class GroupBy(field: String) extends Constraint {
+      val op = "GROUP_BY"
+    }
+  }
 
   case class AppBuilder(
-    id: String, create: Boolean = true,
-    _cmd: Option[String] = None,
-    _cpus: Double = Client.Default.cpus,
-    _mem: Double = Client.Default.mem,
-    _instances: Int = Client.Default.instances,
-    _uris: List[String] = Nil,
-    _env: Map[String, String] = Map.empty[String, String],
+    id: String,
+    create: Boolean                = true,
+    _cmd: Option[String]           = None,
+    _cpus: Double                  = Client.Default.cpus,
+    _mem: Double                   = Client.Default.mem,
+    _instances: Int                = Client.Default.instances,
+    _uris: List[String]            = Nil,
+    _env: Map[String, String]      = Map.empty[String, String],
     _taskRateLimit: Option[Double] = None,
-    _ports: List[Int] = Client.Default.ports,
-    _executor: Option[String] = None,
-    _container: Option[Container] = None,
-    _constraints: Option[List[Constraint]] = None)
+    _ports: List[Int]              = Client.Default.ports,
+    _executor: Option[String]      = None,
+    _container: Option[Container]  = None,
+    _constraints: Option[List[Constraints.Constraint]] = None)
     extends Client.Completion {
 
     def cmd(str: String) = copy(_cmd = Some(str))
@@ -119,31 +150,30 @@ case class Client(
      */
     def taskRateLimit(limit: Double) = copy(_taskRateLimit = Some(limit))
 
+    /** An array of required port resources on the host. To generate one or more arbitrary
+     *  free ports for each application instance, pass zeros as port values.*/
     def ports(ps: Int*) = copy(_ports = ps.toList)
 
     def executor(exec: String) = copy(_executor = Some(exec))
 
     def container(cont: Container) = copy(_container = Some(cont))
 
-    def constraints(consts: Constraint*) = copy(_constraints = Some(consts.toList))
+    def constraints(consts: Constraints.Constraint*) = copy(_constraints = Some(consts.toList))
 
     // future response will have no body
     def apply[T](handler: Client.Handler[T] = as.Unit): Future[T] =
       request((if (create) base.POST / "apps" else base.PUT / "apps" / id)
               << compact(
                 render(("id"  -> Option(create).filter(identity).map(Function.const(id.toString))) ~
-                       ("cmd" -> _cmd) ~
-                       ("cpus" -> _cpus) ~ ("instances" -> _instances) ~
+                       ("cmd" -> _cmd) ~ ("cpus" -> _cpus) ~
+                       ("instances" -> _instances) ~
                        ("mem"  -> _mem)  ~ ("uris"      -> _uris) ~
                        ("taskRateLimit" -> _taskRateLimit) ~
                        ("ports" -> _ports) ~ ("executor" -> _executor) ~
                        ("env"  -> _env) ~ ("container" -> _container.map { c =>
                          ("image" -> c.image) ~ ("options" -> c.options)
                        }) ~ ("constraints" -> _constraints.map { 
-                         _.map {
-                           case Constraint(attr, oper, Some(value)) => List(attr, oper, value)
-                           case Constraint(attr, oper, _) => List(attr, oper)
-                         }
+                         _.map { c => List(c.field, c.op) ++ c.value }
                        }))))(handler)
   }
 
@@ -153,11 +183,11 @@ case class Client(
   def start(appId: String) =
     AppBuilder(appId)
 
-  // repdons with 20
+  // respods with 204 no content
   def update(appId: String) =
     AppBuilder(appId, create = false)
 
-  // responds with 204 no contnt
+  // responds with 204 no content
   def kill(appId: String) =
     complete(base.DELETE / "apps" / appId)
 
